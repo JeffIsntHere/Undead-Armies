768069af47bc2e9a3b34c4b375101decec127144
JeffIsntHere <149245093+JeffIsntHere@users.noreply.github.com>
2025-01-15 13:14:31 +0700

    finished mobGroup safety check

--- src/main/java/undead/armies/util/container/MobGroup.java
+++ src/main/java/undead/armies/util/container/MobGroup.java
@@ -2,32 +2,42 @@ package undead.armies.util.container;
 
 import java.util.ArrayList;
 
-import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.PathfinderMob;
-import net.neoforged.neoforge.event.entity.living.LivingDamageEvent;
-import undead.armies.behaviour.group.Stacking;
 
 public class MobGroup
 {
-    enum GroupTask
-    {
-        stacking,
-        idle
-    }
+    public static final int groupActionCooldown = 10;
     public final ArrayList<PathfinderMob> pathFinderMobArrayList = new ArrayList<>();
     public int tickCount = 0;
+    public int tickMethodCallCounter = 0;
+    public int removePathFinderMobMethodCallCounter = 0;
     public void tick()
     {
-        if(tickCount == 0)
+        this.tickMethodCallCounter++;
+        if(this.tickMethodCallCounter + this.removePathFinderMobMethodCallCounter >= pathFinderMobArrayList.size())
         {
+            if(this.removePathFinderMobMethodCallCounter != 0)
+            {
+                this.cleanPathFinderMobArrayList();
+                this.removePathFinderMobMethodCallCounter = 0;
+            }
+            if(this.tickCount % MobGroup.groupActionCooldown == 0)
+            {
 
+            }
+            this.tickCount++;
+            this.tickMethodCallCounter = 0;
         }
-        tickCount++;
-        if(tickCount >= pathFinderMobArrayList.size())
-        {
-            tickCount = 0;
-        }
-    }/*
+    }
+    public void cleanPathFinderMobArrayList()
+    {
+        this.pathFinderMobArrayList.removeIf(pathFinderMob -> (pathFinderMob.isDeadOrDying()));
+    }
+    public void removePathFinderMob()
+    {
+        this.removePathFinderMobMethodCallCounter++;
+    }
+    /*
     public static void onHitListener(final LivingDamageEvent.Post livingDamageEventPost, final LivingEntity livingEntity, final MobSingle mobSingle)
     {
         if(mobSingle.stack.flag)
